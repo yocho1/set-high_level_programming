a80e56b7d3d3cd665e9bec95ad28448e994b0fdb
Achraf <achraflachgar456@gmail.com>
2026-07-11 17:14:16 -0700

    Fix pycodestyle: add 2 blank lines between functions

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function"""
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function"""
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function"""
+    return (a * b)
+
+
+def div(a, b):
+    """My division function"""
+    return int(a / b)
