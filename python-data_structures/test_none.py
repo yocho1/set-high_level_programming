5534234ec296e147cc3b9ce1ee596d4e782eaf42
Achraf <achraflachgar456@gmail.com>
2026-07-13 15:44:29 -0700

    Fix 3-print_reversed_list_integer.py: handle None type

---
+++ python-data_structures/test_none.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+print_reversed_list_integer(None)  # Should print nothing
+print_reversed_list_integer([1, 2, 3])  # Should print 3 2 1
