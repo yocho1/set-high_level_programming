b306acff52d09fc983abeca3d102905b32792fc9
Achraf <achraflachgar456@gmail.com>
2026-07-21 15:16:44 -0700

    Add module and class docstrings for LockedClass

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a LockedClass with restricted attributes."""
+
+
 class LockedClass:
+    """A class that prevents dynamic attribute creation except first_name."""
     __slots__ = ["first_name"]
