2b179f0c361128c0bb3334c2845e1a31c02ade2a
Achraf <achraflachgar456@gmail.com>
2026-08-13 20:12:48 +0000

    Add City model and script to fetch cities grouped by state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines the City class linked to the cities table."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """Represents a city in the cities MySQL table."""
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", backref="cities")
