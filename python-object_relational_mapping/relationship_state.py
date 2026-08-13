631251fe1a05943e13b3721e8d6033d3c608dc31
Achraf <achraflachgar456@gmail.com>
2026-08-13 20:16:51 +0000

    Fix PEP8 indentation in relationship_state.py

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -15,5 +15,5 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                           cascade="all, delete-orphan")
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan")
