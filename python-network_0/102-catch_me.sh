bd3e222348774ed8e8348a4c642d4c68445c9181
Achraf <achraflachgar456@gmail.com>
2026-08-17 21:19:45 +0000

    Fix catch_me script to use PUT method instead of placeholder

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Sends the correct request to /catch_me to trigger the success message
-curl -s -X <METHOD> "0.0.0.0:5000/catch_me"
+# Sends a PUT request to /catch_me to trigger the success message
+curl -s -X PUT "0.0.0.0:5000/catch_me"
