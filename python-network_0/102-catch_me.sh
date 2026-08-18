15d40cccc0dae22eac05de3d98a1fef553f24164
Achraf <achraflachgar456@gmail.com>
2026-08-18 19:28:31 +0000

    Add 102-catch_me.sh: script to get You got me! response

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-# Sends a PUT request to /catch_me to trigger the success message
-curl -s -X PUT "0.0.0.0:5000/catch_me"
+cript to make server respond with "You got me!"
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
